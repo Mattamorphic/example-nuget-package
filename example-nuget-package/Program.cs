aa3b2ee142630d38b52a55adac2e680ccd0714e0
Mattamorphic <9885259+Mattamorphic@users.noreply.github.com>
2019-07-05 08:54:56 +0100

    Initial commit

---
+++ example-nuget-package/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace example_nuget_package
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
